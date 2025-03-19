40593613cf36d38eba91919f341a295b972d3c73
Fyodor <fiodorlukashov@gmail.com>
2025-03-19 13:36:58 +0200

    Remove admin comments

--- admin_panel/admin_panel/admin.py
+++ admin_panel/admin_panel/admin.py
@@ -6,15 +6,15 @@ from .models import (
 
 class AISettingsInline(admin.TabularInline):
     model = AISettings
-    extra = 0  # Количество пустых форм для добавления новых объектов
-    fields = ('prompt', 'style')  # Поля, которые будут отображаться в таблице
-    readonly_fields = ('created_at', 'updated_at')  # Поля только для чтения (если есть)
+    extra = 0 
+    fields = ('prompt', 'style')
+    readonly_fields = ('created_at', 'updated_at')
 
 class SubscriptionInline(admin.TabularInline):
     model = Subscription
     extra = 0
     fields = ('channel', 'subscription_type', 'end_date', 'is_active')
-    readonly_fields = ('created_at', 'updated_at')  # Поля только для чтения (если есть)
+    readonly_fields = ('created_at', 'updated_at')
 
 
 @admin.register(User)
