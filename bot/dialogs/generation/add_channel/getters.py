5dd530701e4dde4e0eccf9cef5cec3e31157e1bb
Fyodor <fiodorlukashov@gmail.com>
2025-04-01 19:30:32 +0300

    Update bot add to channel url

--- bot/dialogs/generation/add_channel/getters.py
+++ bot/dialogs/generation/add_channel/getters.py
@@ -5,5 +5,5 @@ async def channel_data_getter(dialog_manager: DialogManager, **kwargs):
     me = await bot.get_me()
     return {
         "bot_username": me.username,
-        "bot_url": f"https://t.me/{me.username}?startchannel=admin"
+        "bot_url": f"https://t.me/{me.username}?startchannel&admin=post_messages+edit_messages+delete_messages"
     }
