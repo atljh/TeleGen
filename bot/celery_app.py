823f71ed34bd79920aa5d45076d6ad6dd538f9a6
Fyodor <fiodorlukashov@gmail.com>
2025-04-15 23:23:54 +0300

    Rename celery app

--- bot/celery_app.py
+++ bot/celery_app.py
@@ -1,8 +1,10 @@
-from celery import Celery
 import os
+from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'admin_panel.core.settings')
 
 app = Celery('bot')
+
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks(['bot.tasks'])
+
+app.autodiscover_tasks()
