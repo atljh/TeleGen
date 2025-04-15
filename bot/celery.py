79326953aef768dc90d49f57f7344d7dcba56b5f
Fyodor <fiodorlukashov@gmail.com>
2025-04-15 23:08:21 +0300

    Add celery config

--- bot/celery.py
+++ bot/celery.py
@@ -1,3 +1,8 @@
 from celery import Celery
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 app = Celery('bot')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
