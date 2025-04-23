9fb3abba29780e424b31ff1398bc99e85d0e95f6
Fyodor <fiodorlukashov@gmail.com>
2025-04-23 20:36:08 +0300

    Create user by start command

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -2,10 +2,18 @@ from aiogram import Router, F
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 from aiogram.filters import Command
 
+from bot.containers import Container
+
 router = Router()
+
 
 @router.message(Command("start"))
 async def cmd_start(message: Message):
+    user_service = Container.user_service()
+    user_dto, created = await user_service.create_or_get_user(
+        telegram_id=message.from_user.id,
+        username=message.from_user.username
+    )
     keyboard = ReplyKeyboardMarkup(
         keyboard=[
             [KeyboardButton(text="✨ Генерацiя"), KeyboardButton(text="📂 Буфер")],
