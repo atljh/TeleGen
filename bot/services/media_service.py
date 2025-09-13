228650a6542d263a1636af832b0371c9e219fb85
Fyodor <fiodorlukashov@gmail.com>
2025-09-13 13:27:34 +0300

    Fix image path

--- bot/services/media_service.py
+++ bot/services/media_service.py
@@ -72,7 +72,7 @@ class MediaService:
         return ".jpg" if media_type == "images" else ".mp4"
 
     def _get_permanent_media_path(self, media_type: str, extension: str) -> str:
-        media_dir = self.image_dir if media_type == "images" else self.video_dir
+        media_dir = self.image_dir if media_type == "image" else self.video_dir
         logger.warning(f'{media_dir} | {media_type} | {extension}')
         permanent_dir = os.path.join(settings.MEDIA_ROOT, media_dir)
         os.makedirs(permanent_dir, exist_ok=True)
