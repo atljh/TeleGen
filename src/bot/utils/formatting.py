9f95b298e6ebb2b923cfdc1fbc93291957af89a7
Fyodor <fiodorlukashov@gmail.com>
2025-10-03 14:23:08 +0300

    Fix signature input to handle more than 2 formants in row

--- src/bot/utils/formatting.py
+++ src/bot/utils/formatting.py
@@ -1,27 +1,6 @@
 from html import escape as escape_html
 
-from aiogram.types import Message, MessageEntity
-
-
-def entity_to_html(entity: MessageEntity, text: str) -> str:
-    if entity.type == "bold":
-        return f"<b>{escape_html(text)}</b>"
-    elif entity.type == "italic":
-        return f"<i>{escape_html(text)}</i>"
-    elif entity.type == "underline":
-        return f"<u>{escape_html(text)}</u>"
-    elif entity.type == "strikethrough":
-        return f"<s>{escape_html(text)}</s>"
-    elif entity.type == "code":
-        return f"<code>{escape_html(text)}</code>"
-    elif entity.type == "pre":
-        return f"<pre>{escape_html(text)}</pre>"
-    elif entity.type == "text_link":
-        return f'<a href="{escape_html(entity.url)}">{escape_html(text)}</a>'
-    elif entity.type == "url":
-        return f'<a href="{escape_html(text)}">{escape_html(text)}</a>'
-    else:
-        return escape_html(text)
+from aiogram.types import Message
 
 
 def parse_entities_to_html(message: Message) -> str:
@@ -96,12 +75,7 @@ def apply_formats(text: str, formats: list, all_entities: list) -> str:
         elif fmt == "text_link":
             url = None
             for entity in all_entities:
-                if (
-                    entity.type == "text_link"
-                    and entity.offset
-                    <= text.find(text[0])
-                    < entity.offset + entity.length
-                ):
+                if entity.type == "text_link":
                     url = entity.url
                     break
             if url:
